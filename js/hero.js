7f2b2d1ab8ca2ec94225ebcfae02beda6c55ccb7
xxOriiana <87913312+xxOriiana@users.noreply.github.com>
2023-04-04 08:36:45 -0700

    Add files via upload

---
+++ js/hero.js
@@ -0,0 +1,22 @@
+const text = document.querySelector("#text");
+const cursor = document.querySelector("#cursor");
+let textIndex = 0;
+let cursorInterval;
+let textDuration = 50;
+const originalText = text.innerHTML;
+
+function typeText() {
+  if (textIndex < originalText.length) {
+    text.innerHTML = originalText.substring(0, textIndex+1) + "_";
+    textIndex++;
+    setTimeout(typeText, textDuration);
+  } else {
+    text.innerHTML = originalText.substring(0, textIndex).replace("_", "");
+    clearInterval(cursorInterval);
+    cursorInterval = setInterval(() => {
+      cursor.style.opacity = cursor.style.opacity == 0 ? 1 : 0;
+    }, 500);
+}
+}
+
+typeText();
