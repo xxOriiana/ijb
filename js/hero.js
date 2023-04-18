a9def66b21c6f838abbf0a4dcbef92e38f6e05a4
xxOriiana <87913312+xxOriiana@users.noreply.github.com>
2023-04-18 00:22:19 -0700

    Add files via upload

--- js/hero.js
+++ js/hero.js
@@ -2,7 +2,7 @@ const text = document.querySelector("#text");
 const cursor = document.querySelector("#cursor");
 let textIndex = 0;
 let cursorInterval;
-let textDuration = 30;
+let textDuration = 18;
 const originalText = text.innerHTML;
 
 function typeText() {
